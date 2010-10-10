84e5ebaae6597e5b5dfb0aef673de242473a1d72
gongdewei <gongdewei@8edf365c-d698-11de-82c3-17aed804ed57>
2010-10-10 01:18:13 +0000

    提交common包代码

---
+++ javaxyq-common/src/com/javaxyq/data/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package com.javaxyq.data.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
