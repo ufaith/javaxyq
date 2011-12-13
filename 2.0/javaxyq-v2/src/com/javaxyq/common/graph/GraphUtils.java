343fd5639b0fbaa8317f0d7f41ab86817237372f
gongdewei <gongdewei@8edf365c-d698-11de-82c3-17aed804ed57>
2011-12-13 16:24:42 +0000

    导入javaxyq v2.0

---
+++ 2.0/javaxyq-v2/src/com/javaxyq/common/graph/GraphUtils.java
@@ -0,0 +1,31 @@
+﻿/**
+ * 
+ */
+package com.javaxyq.common.graph;
+
+import java.awt.Image;
+import java.io.IOException;
+
+import com.javaxyq.common.io.IOUtils;
+
+/**
+ * @author gongdewei
+ * @date 2011-8-14 create
+ */
+public class GraphUtils {
+
+    public static Image createImageFromResource(String filename) {
+        byte[] data = null;
+        try {
+            data = IOUtils.getResourceData(filename);
+        } catch (IOException e) {
+            System.err.println("create image error!");
+            e.printStackTrace();
+        }
+        if (data == null) {
+            return null;
+        }
+        return java.awt.Toolkit.getDefaultToolkit().createImage(data);
+    }
+
+}
